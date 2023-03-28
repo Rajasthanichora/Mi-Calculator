94ac94acc9f603cfa7f6baeb9b5c37398d104678
RAVI <124796706+Rajasthanichora@users.noreply.github.com>
2023-03-28 20:06:20 +0530

    dark mode all code replace

--- script.js
+++ script.js
@@ -25,7 +25,44 @@ function valuebuttun(e) {
     document
       .getElementById("inputbox")
       .setAttribute("style", "font-size: 28px;  height: 35px;");
-
+    if (input2.value.length == 10) {
+      document.getElementById("inputbox2u").style.fontSize = "40px";
+    } else if (input2.value.length == 11) {
+      document.getElementById("inputbox2u").style.fontSize = "38px";
+    } else if (input2.value.length == 12) {
+      document.getElementById("inputbox2u").style.fontSize = "36px";
+    } else if (input2.value.length == 13) {
+      document.getElementById("inputbox2u").style.fontSize = "34px";
+    } else if (input2.value.length == 14) {
+      document.getElementById("inputbox2u").style.fontSize = "32px";
+    } else if (input2.value.length == 15) {
+      document.getElementById("inputbox2u").style.fontSize = "30px";
+    } else if (input2.value.length == 16) {
+      document.getElementById("inputbox2u").style.fontSize = "28px";
+    } else if (input2.value.length == 17) {
+      document.getElementById("inputbox2u").style.fontSize = "27px";
+    } else if (input2.value.length == 18) {
+      document.getElementById("inputbox2u").style.fontSize = "26px";
+    } else if (input2.value.length == 19) {
+      document.getElementById("inputbox2u").style.fontSize = "25px";
+    } else if (input2.value.length == 20) {
+      document.getElementById("inputbox2u").style.fontSize = "24px";
+    }
+    document.getElementById("inputbox2u").style.marginBottom = "6px";
+    if (input2.value == "undefined=") {
+      document.getElementById("inputbox2u").style.color = "white";
+      document.getElementById("inputbox2u").style.marginBottom = "6px";
+    }
+    // ! MY SIGNATURE START
+    if (input.value == "%6350%") {
+      alert("MADE BY『』𝑅𝒜𝒥𝒜𝒮𝒯𝐻𝒜𝒩𝐼 𝒞𝐻𝒪𝑅𝒜");
+      document.getElementById("inputbox2u").value =
+        "MADE BY『』𝑅𝒜𝒥𝒜𝒮𝒯𝐻𝒜𝒩𝐼 𝒞𝐻𝒪𝑅𝒜";
+      document.getElementById("inputbox").value = "🎀  𝒲𝐸𝐿𝒞♡𝑀𝐸  🎀";
+      document.getElementById("inputbox2u").style.fontSize = "15px";
+      document.getElementById("inputbox2u").style.color = "black";
+    }
+    // ! MY SIGNATURE START
     // !ac buttun
   } else if (buttonText == "AC") {
     input.value = null;
@@ -38,14 +75,13 @@ function valuebuttun(e) {
   } else if ((input.value += buttonText)) {
     document
       .getElementById("inputbox2u")
-      .setAttribute("style", "color: black;");
+      .setAttribute("style", "color: #7b7b7b;");
   } else {
     input.value += buttonText;
     input.value = input.value;
   }
   input2.value = eval(input.value);
   input2.value += "=";
-
   // ! Text overflow start
   // ? input 1 start
   if (input.value.length == 10) {
@@ -69,136 +105,42 @@ function valuebuttun(e) {
   // ? input 2 end
   // ! Text overflow end
 }
-
 // ! page reload start
 if (input2.value == "undefined=") {
   document.getElementById("inputbox2u").style.color = "white";
 }
 // ! page reload end
-
 // ! backspace buttun start
 function backspace() {
   input.value = input.value.slice(0, -1);
   input2.value = input2.value.slice(0, -1);
 }
 // ! backspace buttun end
-
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
-// ! Dark mode start
-function Darkmode() {
-  /////////////////////////////////////////////////
-  // ? numbers digits 1----9 start
-  var whiteclrbtn = document.querySelectorAll(".modebtn");
-  for (let i = 0; i < whiteclrbtn.length; i++) {
-    whiteclrbtn[i].style.color = "white";
-  }
-  // ? numbers digits 1----9 end
-  /////////////////////////////////////////////////
-  // !input color start
-  var blackclrbtn = document.querySelectorAll("input");
-  for (let i = 0; i < blackclrbtn.length; i++) {
-    blackclrbtn[i].style.backgroundColor = "black";
-  }
-  // * equal === buttun start
-  var darkinputs = document.getElementById("equalbtn");
-  darkinputs.addEventListener("click", clickdark);
-  function clickdark() {
-    document
-      .getElementById("inputbox2u")
-      .setAttribute(
-        "style",
-        "font-size: 55px;  height: 40px; margin-bottom: 10px; transition: 0.4s; "
-      );
-    document
-      .getElementById("inputbox")
-      .setAttribute("style", "font-size: 28px;  height: 35px;");
-    document.getElementById("inputbox").style.backgroundColor = "black";
-    document.getElementById("inputbox2u").style.backgroundColor = "black";
-    document.getElementById("inputbox2u").style.color = "white";
-    document.getElementById("inputbox").style.color = "white";
-    if (input2.value == "undefined=") {
-      document.getElementById("inputbox2u").style.color = "black";
-    }
-  }
-  // * equal === buttun end
-  // * all keysssss buttun start
-  var allkeysdarkinput = document.querySelectorAll(".keybutton");
-  for (let i = 0; i < allkeysdarkinput.length; i++) {
-    allkeysdarkinput[i].addEventListener("click", allkeysclickdrk);
-    function allkeysclickdrk() {
-      document.getElementById("inputbox").style.backgroundColor = "black";
-      document.getElementById("inputbox2u").style.backgroundColor = "black";
+function Darklightmode() {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    var eqauall = document.getElementById("equalbtn");
+    eqauall.addEventListener("click", function () {
       document.getElementById("inputbox2u").style.color = "white";
-      document.getElementById("inputbox").style.color = "white";
-      if (input2.value == "undefined=") {
-        document.getElementById("inputbox2u").style.color = "black";
+    });
+    document.getElementById("lightbtnn").style.display = "contents";
+    document.getElementById("darkbtnn").style.display = "none";
+    var alldugits = document.querySelectorAll(".modebtn");
+    for (let i = 0; i < alldugits.length; i++) {
+      document.addEventListener("click", clikingdigit);
+      function clikingdigit() {
+        if (input2.value == "undefined=") {
+          document.getElementById("inputbox2u").style.color = "black";
+        }
       }
     }
-  }
-  // * all keysssss buttun end
-  // * AC buttun start
-  var acbtndarkinputs = document.getElementById("Acbuttun");
-  acbtndarkinputs.addEventListener("click", acclickdark);
-  function acclickdark() {
-    document.getElementById("inputbox").style.backgroundColor = "black";
-    document.getElementById("inputbox2u").style.backgroundColor = "black";
-    document.getElementById("inputbox2u").style.color = "black";
-    document.getElementById("inputbox").style.color = "black";
-    if (input2.value == "undefined=") {
-      document.getElementById("inputbox2u").style.color = "black";
-    }
-  }
-  // * Ac buttun end
-  // !input color end
-  /////////////////////////////////////////////////
-  //  ? convert section start
-  document.getElementById("convertsection").style.backgroundColor = "black";
-  document.getElementById("convertsection").style.border = "none";
-  //  ? convert section end
-  /////////////////////////////////////////////////
-  //  ? backspace buttun start
-  document.getElementById("modebacksace").style.backgroundColor = "black";
-  //  ? backspace buttun end
-  /////////////////////////////////////////////////
-  // * switch butttun dark && white
-  //  ? white mode buttun start
-  document.getElementById("lightbtnn").style.display = "contents";
-  //  ? white mode buttun end
-  /////////////////////////////////////////////////
-  //  ? dark mode buttun start
-  document.getElementById("darkbtnn").style.display = "none";
-  //  ? dark mode buttun end
-  /////////////////////////////////////////////////
-  //  ? dark mode --&&- white mode buttun start
-  document.getElementById("darkmodebtn").style.backgroundColor = "black";
-  //  ? dark mode --&&- white mode buttun end
-  // * switch butttun dark && white
-  /////////////////////////////////////////////////
-  //  ? equal buttun start
-  document.getElementById("equalbtn").style.backgroundColor = "#ca5d0e";
-  //  ? equal buttun end
-  /////////////////////////////////////////////////
-  // ? hover background effect start
-  var allkeys = document.querySelectorAll(".keybutton");
-  for (let i = 0; i < allkeys.length; i++) {
-    allkeys[i].addEventListener("mouseover", mouseOver);
-    allkeys[i].addEventListener("mouseout", mouseOut);
-    function mouseOver() {
-      allkeys[i].style.backgroundColor = "#222";
-    }
-    function mouseOut() {
-      allkeys[i].style.backgroundColor = "black";
-    }
-  }
-  // ? hover background effect end
-  /////////////////////////////////////////////////
-  if (input2.value == "undefined=") {
-    document.getElementById("inputbox2u").style.color = "black";
+  } else {
+    document.getElementById("darkbtnn").style.display = "contents";
+    document.getElementById("lightbtnn").style.display = "none";
   }
 }
-// ! Dark mode end
-
